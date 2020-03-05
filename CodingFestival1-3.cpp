8e261f203475d9db176205d850213924c456a3b8
wack.man <61845083+deconleo@users.noreply.github.com>
2020-03-05 10:59:52 -0800

    Create CodingFestival1-3.cpp

---
+++ CodingFestival1-3.cpp
@@ -0,0 +1,30 @@
+// Mekhi Woods 2.26.20
+#include <iostream>
+using namespace std;
+
+void intProcessing(int procOne, int procTwo, int&procMod)
+{
+procMod = procOne % procTwo;
+}
+
+void primDisplay(int dispOne, int dispTwo, int dispMod)
+{
+cout << dispOne << " % " << dispTwo << " = " << dispMod << endl
+<< "AKA... the remainder of " << dispOne << " / " << dispTwo << " = " << dispMod << endl
+<< "BYE -primDisplay\n";
+}
+
+int main()
+{
+int userOne, userTwo, userMod;
+userMod = 0;
+cout << "Please enter two integers:\n";
+cin >> userOne >> userTwo;
+
+intProcessing(userOne, userTwo, userMod);
+primDisplay(userOne, userTwo, userMod);
+
+cout << "AIDOS -main\n";
+
+return 0;
+}
